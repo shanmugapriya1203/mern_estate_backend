38a169dccf3894aa6330ca98d0970794e4c867c3
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-06 12:04:57 +0530

    create a signin api route

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user_model.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
+
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -13,3 +14,22 @@ export const signup = async (req, res,next) => {
   next(error.message)
   }
 };
+
+export const signin= async(req,res)=>{
+    const {email,password}= req.body
+    try {
+        const exisistingUser= await User.findOne({ email})
+        if(!exisistingUser){
+            return res.status(404).json({ success: false, message: 'User not found' });
+
+        }
+        const validPassword= bcryptjs.compareSync(password,exisistingUser.password);
+        if(!validPassword) return res.status(401).json({success:false,messaeg:'Invalid Credentials'})
+
+        const token= jwt.sign({id : exisistingUser._id},process.env.JWT_SECRET)
+        const {password :pass, ...rest}= exisistingUser._doc
+    res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
+    } catch (error) {
+        next(error.message)
+    }
+}
--- server.js
+++ server.js
@@ -1,42 +1,49 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import userrouter from './api/routes/user_route.js'
-import authRouter from './api/routes/auth_route.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import cors from 'cors';
+import bodyParser from 'body-parser'; // Import body-parser
+import userrouter from './api/routes/user_route.js';
+import authRouter from './api/routes/auth_route.js';
+import dotenv from 'dotenv';
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Mongoose default connection is done')
-})
-.catch((err)=>{
-    console.log(err)
-})
+dotenv.config();
 
-const app= express();
-app.use(express.json())
-app.use((err,req,res,next)=>{
-    const statusCode= err.statusCode || 500;
-    const message=err.message || "Internal Server Error"
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
-})
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Mongoose default connection is done');
+}).catch((err) => {
+  console.log(err);
+});
 
-app.listen(8000,()=>{
-    console.log('Server is running on Port 8000 !')
-})
+const app = express();
 
 
-app.get('/',(req,res)=>{
-    res.json({
-        message: "Welcome to the Real Estate API!",
-        description: "This API powers the Real Estate application's backend.",
-        version: "1.0"
-      });
-    
-})
+app.use(cors());
 
-app.use('/api/user',userrouter)
-app.use('/api/auth',authRouter)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on Port 8000!');
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "Welcome to the Real Estate API!",
+    description: "This API powers the Real Estate application's backend.",
+    version: "1.0"
+  });
+});
+
+app.use('/api/user', userrouter);
+app.use('/api/auth', authRouter);
