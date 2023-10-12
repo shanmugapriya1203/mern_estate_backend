3befc2fb5de2ef62ca434d93f50c700a098d43ea
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-12 21:21:06 +0530

    create UpdateListing api route

--- api/routes/listing_route.js
+++ api/routes/listing_route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createListing, deleteListing } from '../controllers/listing_controller.js'
+import { createListing, deleteListing, updateListing } from '../controllers/listing_controller.js'
 
 
 const router= express.Router()
 
 router.post('/create',createListing)
 router.delete('/delete/:id',deleteListing)
+router.post('/edit/:id',updateListing)
 export default router
