447c2d8f4fd8a8407d121a4a262091d20f2ac216
Sam <priyamuthukumar1203@gmail.com>
2024-07-25 11:07:50 +0530

    Update auth_controller.js

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -47,7 +47,7 @@ export const signin = async (req, res) => {
 
 
 
-
+//google controller
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
